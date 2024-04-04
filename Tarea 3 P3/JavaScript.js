1137e2f96809e1347915404db2b78e4944050c80
HermesM7 <113647180+HermesM7@users.noreply.github.com>
2024-04-04 00:22:19 -0400

    Update Logic of JavaScript.js

--- Tarea 3 P3/JavaScript.js
+++ Tarea 3 P3/JavaScript.js
@@ -1,24 +1,40 @@
 function convertir() {
-    
+
     let valor = parseFloat(document.getElementById("inputValue").value);
     let unidadDesde = document.getElementById("unitFrom").value;
+    let unidadHasta = document.getElementById("unitTo").value;
 
-    
     let resultado;
     switch (unidadDesde) {
         case "m":
-            resultado = valor * 3.28084; 
+            if (unidadHasta === "cm") {
+                resultado = valor * 100; 
+            } else if (unidadHasta === "ft") {
+                resultado = valor * 3.28084; 
+            } else if (unidadHasta === "km") {
+                resultado = valor * 0.001; 
+            } else if (unidadHasta === "in") {
+                resultado = valor * 39.3701; 
+            } else {
+                resultado = valor; 
+            }
             break;
         case "cm":
-            resultado = valor * 0.0328084; 
+            
             break;
         case "ft":
-            resultado = valor * 0.3048; 
+            
             break;
-   
+        case "km":
+            
+            break;
+        case "in":
+            
+            break;
         default:
             resultado = "Unidad no válida";
     }
-    
+
+
     document.getElementById("result").innerHTML = resultado.toFixed(2);
 }
